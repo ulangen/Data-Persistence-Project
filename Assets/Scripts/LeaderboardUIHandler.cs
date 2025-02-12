47de8d24c1311b3964ddaafc69309b9ca685b43f
Vladimir Batasov <ulangen.dev@gmail.com>
2025-02-13 02:55:23 +0300

    Rename LeaderboardUIManager to LeaderboardUIHandler

--- Assets/Scripts/LeaderboardUIHandler.cs
+++ Assets/Scripts/LeaderboardUIHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LeaderboardUIManager : MonoBehaviour
+public class LeaderboardUIHandler : MonoBehaviour
 {
     public List<Text> m_LeaderboardTextItems;
 
