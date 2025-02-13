f35be16dde58471e8b9cfcb08d484872ef58d3be
Vladimir Batasov <ulangen.dev@gmail.com>
2025-02-13 03:26:28 +0300

    Add leaderboard error message

--- Assets/Scripts/LeaderboardUIHandler.cs
+++ Assets/Scripts/LeaderboardUIHandler.cs
@@ -6,11 +6,22 @@ using UnityEngine.UI;
 
 public class LeaderboardUIHandler : MonoBehaviour
 {
-    public List<Text> m_LeaderboardTextItems;
+    public List<Text> LeaderboardTextItems;
+    public Text ErrorMessage;
 
     private void Start()
     {
+        if (DataManager.Instance == null)
+        {
+            ShowLoadDataError();
+            return;
+        }
+    }
 
+    private void ShowLoadDataError()
+    {
+        ErrorMessage.gameObject.SetActive(true);
+        ErrorMessage.text = "Oops... Filed to load data.";
     }
 
     public void BackToMenu()
